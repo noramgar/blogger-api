654c08995b239e3f1d4dc478be3d53951111e7aa
Noe <noramgar@protonmail.com>
2020-11-22 01:53:56 -0500

    add router files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import express from 'express';
+import express from 'express'
+import userRouter from './resources/user/user.router'
+import postRouter from './resources/post/post.router'
+import categoryRouter from './resources/category/category.router'
+import postCategoryRouter from './resources/post-category/post-category.router'
+import commentRouter from './resources/comment/comment.router'
 
 const app = express()
 const port = 3000
+
+app.use('/Users', userRouter)
+app.use('/Posts', postRouter)
+app.use('/Categories', categoryRouter)
+app.use('/PostCategory', postCategoryRouter)
+app.use('/Comments', commentRouter)
 
 app.get('/', (req, res) => {
   res.send('Goodbye World')
